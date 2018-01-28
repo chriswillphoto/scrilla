a8a2844848ef0b97c27ce0872f807a44c742732c
chriswillphoto <christophernekich@gmail.com>
2018-01-28 21:15:10 +1100

    got rid of dead code

--- src/components/BudgetItem.js
+++ src/components/BudgetItem.js
@@ -7,8 +7,7 @@ const BudgetItem = (props) => {
   console.log(props)
   return(
     <div className="budget-item">
-      <h2 className="budget-item-name">{props.item.name}</h2>
-      {/* <span>-</span> */}
+      <h2 className="budget-item-name">{props.item.name}</h2
       <h2 className="budget-item-recurrence">{props.item.recurrence}</h2>
       <h3>${props.item.amount}</h3>
     </div>
