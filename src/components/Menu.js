b8b262d4661d774e396c6315969b1c6d44196dd4
chriswillphoto <christophernekich@gmail.com>
2018-05-13 09:23:52 +1000

    test

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,7 +5,7 @@ const Menu = (props) => {
   return (
     <div className={"splash " + (props.splashActive ? "" : "shrink")}>
       <div className={"splash-words " + (props.splashActive ? "" : "hide")}>
-        <h1 id="title">Scrilla</h1>
+        <h1 id="title">Scrillas</h1>
         <h2>Visualise where your money goes</h2>
         <button onClick={props.shrinkMenu}>Let's Go</button>
       </div>
